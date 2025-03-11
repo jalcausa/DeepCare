d71630011acfc08ad5ae19c0e50f886cd5c7f995
jcalcausal <jcalcausal@gmail.com>
2025-03-11 18:58:37 +0100

    Example

---
+++ example.py
@@ -0,0 +1,13 @@
+from secrets import API_KEY
+
+import openai # openai v1.0.0+
+client = openai.OpenAI(api_key=API_KEY,base_url="https://litellm.dccp.pbu.dedalus.com") # set proxy to base_url
+# request sent to model set on litellm proxy, `litellm --model`
+response = client.chat.completions.create(model="bedrock/anthropic.claude-3-5-sonnet-20240620-v1:0", messages = [
+    {
+        "role": "user",
+        "content": "this is a test request, write a short poem"
+    }
+])
+ 
+print(response)
