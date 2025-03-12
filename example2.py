9abad5f9193f24287524ddeafa39d26ab7e80ae3
juanferjimenez <juanfernandojc@gmail.com>
2025-03-12 10:40:47 +0100

    Archivo example2, prueba Gemini

---
+++ example2.py
@@ -0,0 +1,19 @@
+# GEMINI PRO
+
+from secret import GEMINI_KEY
+import requests
+import json
+
+import google.generativeai as genai
+
+# Configurar la API de Gemini
+genai.configure(api_key=GEMINI_KEY)
+
+def newPrompt(petition):
+    model = genai.GenerativeModel("gemini-1.5-pro-latest")  # Usa Gemini Pro
+    response = model.generate_content(
+        petition
+    )
+    return response.text  # Extrae el texto de la respuesta
+
+print(newPrompt("Write a short poem"))
