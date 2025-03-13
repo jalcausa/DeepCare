6d01b078e9db02385a16c80facfd661c9d43646c
jcalcausal <jcalcausal@gmail.com>
2025-03-13 12:03:59 +0100

    Pruebas prompt

--- src/graph_generator.py
+++ src/graph_generator.py
@@ -11,12 +11,19 @@ model = genai.GenerativeModel("gemini-2.0-flash")
 def generar_codigo_grafico(peticion):
     """Genera un código en Python basado en la petición del usuario usando Gemini"""
     
-    prompt = f"""Genera un código en Python que grafique datos.
-    La petición del usuario es: {peticion}.
+    prompt = f"""Genera un código en Python que grafique datos.  
+	La petición del usuario es: {peticion}.  
+
+	Usa pandas para leer el archivo. Ten en cuenta que puede haber filas con columnas sin valores y debes manejarlo correctamente. Usa `na_values=[""]` y `keep_default_na=False` para tratar los valores vacíos correctamente.  
+
+	Asegúrate de que la columna de ID de paciente se llame `PacienteID` y que `Fecha` y `Hora` se combinen en una nueva columna `fecha_hora` con `pd.to_datetime()`.
     
-    Usa pandas para leer el archivo y seaborn/matplotlib para graficar.
-    Asegúrate de que el código sea ejecutable y claro.
-    No incluyas explicaciones, solo el código. La primera línea debe empezar con import.
+    Si alguna línea tiene más campos de los que debería considerala no válida y no la incluyas en el gráfico.
+	Usa seaborn/matplotlib para graficar los datos.  
+
+	Asegúrate de que el código sea ejecutable y claro.  
+	No incluyas explicaciones, solo el código. La primera línea debe empezar con `import`.  
+
     """
 
     respuesta = model.generate_content(prompt)
@@ -40,7 +47,7 @@ def ejecutar_codigo(codigo):
     except Exception as e:
         print(f"Error al ejecutar el código: {e}")
 
-'''
+
 # Ejemplo de uso:
 peticion_usuario = input("¿Qué quieres visualizar?\n")
 codigo_generado = generar_codigo_grafico(peticion_usuario)
@@ -49,5 +56,5 @@ codigo_limpio = limpiar_codigo(codigo_generado)
 print("Código generado:\n", codigo_generado)
 print("Código limpio:\n ", codigo_limpio)
 
-ejecutar_codigo(codigo_limpio)'
-'''
+ejecutar_codigo(codigo_limpio)
+
