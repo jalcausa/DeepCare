38596e833c39f5eff2bf478666ec919978e9bb8a
jcalcausal <jcalcausal@gmail.com>
2025-03-13 11:23:28 +0100

    Minor change

--- src/graph_generator.py
+++ src/graph_generator.py
@@ -40,13 +40,14 @@ def ejecutar_codigo(codigo):
     except Exception as e:
         print(f"Error al ejecutar el código: {e}")
 
-
+'''
 # Ejemplo de uso:
-peticion_usuario = input("¿Qué quieres visualizar?")
+peticion_usuario = input("¿Qué quieres visualizar?\n")
 codigo_generado = generar_codigo_grafico(peticion_usuario)
 codigo_limpio = limpiar_codigo(codigo_generado)
 
 print("Código generado:\n", codigo_generado)
 print("Código limpio:\n ", codigo_limpio)
 
-ejecutar_codigo(codigo_limpio)
+ejecutar_codigo(codigo_limpio)'
+'''
